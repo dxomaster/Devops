5627660afbfb5c495c2f0dbdfabc7da49293c349
eden97bar <eden97.bar@gmail.com>
2023-04-19 14:51:00 +0300

    not eden bar

--- edenbar.java
+++ edenbar.java
@@ -2,7 +2,7 @@ public class edenbar {
     public static void main(String[]args){
         for(int i=1;i<=10;i++)
         {
-            System.out.println("Eden Bar "+i);
+            System.out.println("not Eden Bar :) "+i);
             
         }
     }
